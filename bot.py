93c63c0f89579b5e6ebe1ea11cc04854cb678ec7
tlstkr <tlstkr@icloud.com>
2021-07-15 22:32:08 +0300

    modified the way result image creation

--- bot.py
+++ bot.py
@@ -1,24 +1,27 @@
 import telebot
-#from telebot.apihelper import send_message
 import config
 import model
+# -*- coding: utf-8 -*-
 
 
 bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands = ['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'Hello! I am the handwritten text generation bot using machine learning. Unfortunately, as for now I\'m small and can generate word by word. I am only learning to generate big textes. So, send me a word to generate it into handwritten: ')
+    bot.send_message(message.chat.id, 'Hello! I am the handwritten text generation bot using machine learning. So, send me a text to generate it into handwritten: ')
 
 @bot.message_handler(content_types = ['text'])
 def predict(message):
 
     message.text = message.text.lower()
-    if model.check_input_validation(message.text):
+    text_validation = model.check_input_validation(message.text)
+    if text_validation == True:
         model.generate_result(message.text)
-        bot.send_photo(message.chat.id, photo=open('result.jpg', 'rb'))
+        bot.send_photo(message.chat.id, photo=open('result.png', 'rb'))
+
     else:
-        bot.send_message(message.chat.id, 'Please, send me a text containing only characters and spaсes')
+        bot.send_message(message.chat.id, 'Prohibited chars: ' + text_validation)
+    
         
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -1,52 +1,124 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[7]:
+
 
 import tensorflow as tf
 import numpy as np
 from PIL import Image
 
 
-def check_input_validation(word):
-    iter = 0
-    for i in word:
-        if i in 'abcdefghijklmnopqrstuvwxyz ':
-            iter += 1
-    return iter == len(word)
+# In[8]:
+
+
+def check_input_validation(text):
+    chars = ''
+    for i in text:
+        if i not in 'abcdefghijklmnopqrstuvwxyz ':
+            chars += i
+    if len(chars) == 0:
+        return True
+    else:
+        return chars
+
+
+
+# In[9]:
+
 
 models = {}
 def get_image(letter):
 
     if letter not in list(models.keys()):
-        models[letter] = tf.keras.models.load_model(letter+'_generator')
+        models[letter] = tf.keras.models.load_model('generators/'+letter+'_generator')
         
     fixed_noise = np.random.normal(0, 1, size=(100, 100))
     imgs = models[letter].predict(fixed_noise)
     img = imgs[np.random.randint(1,len(imgs))-1]
-    img = tf.image.resize(img, (200, 200), antialias=True)
+    img = tf.image.resize(img, (28, 28), antialias=True)
     # print("letter:", img.size)
     return img
 
 
-def generate_result(word):
-    images = []
-    result_image = np.full((28,200,1), -1)
-    for i in word:
-        if i == ' ':
-            result_image = np.concatenate((result_image, np.full((28,200,1), -1)), axis=1)
-        else:
-            result_image = np.concatenate((result_image, get_image(i)), axis=1)
-    result_image = np.concatenate((result_image, np.full((28,200,1), -1)), axis=1)
-    # result_image = tf.image.resize(result_image, (1200, 1200))
+
+def generate_result(text):
+    
+    text = text.lower()
+    
+    string_len = int(np.sqrt(len(text)))
+    
+    if string_len < 20:
+        string_len = 20
+        
+    if len(text) < 20:
+        string_len = len(text)
+    
+    def break_text(text, string_len = 28):
+        
+        strings = []
+        string = ''
+        words = text.split()
+        
+        for word in words:
+            if len(word) < string_len:
+                if len(string + word) <= string_len:
+                    string += word 
+                    if len(string) < string_len:
+                        string += ' '
+                    
+                elif len(string + word) == string_len:
+                    string += word
+                    
+                else:
+                    strings.append(string)
+                    string = word + ' '
+            
+            elif len(word) == string_len:
+                if len(string) > 0:
+                    strings.append(string)
+                string = word
+            else:
+                if len(string) > 0:
+                    strings.append(string)
+                    
+                if len(word) > string_len:
+                    for i in range(int(len(word) / string_len)):
+                        strings.append(word[string_len * i : string_len * (i+1)])
+                        
+                string = word[-(len(word) % string_len):] + ' '
+        strings.append(string)
+
+        return strings
+    
+    def generate_string(string, string_len = 28):
+        
+        space = np.full((28,28,1), -1)
+        result = space
+        chars = 0
+        
+        for char in string:
+            if char == ' ':
+                result = np.concatenate((result, space), axis = 1)
+            else:
+                result = np.concatenate((result, get_image(char)), axis = 1)
+            chars += 1
+        
+        extra_space = np.full((28, 28 * (string_len - chars), 1), -1)
+        result = np.concatenate((result, extra_space), axis = 1)
+        return result
+
+    
+    
+    strings = []
+    strings.append(np.full((28,28 * (string_len + 1),1), -1))
+    for string in break_text(text, string_len):
+        strings.append(generate_string(string, string_len))
+
+    strings.append(np.full((28,28 * (string_len + 1),1), -1))
+    result_image = np.concatenate(strings, axis=0)
     result_image = tf.keras.preprocessing.image.array_to_img(result_image)
-    # result_image.thumbnail((1200, 1200),Image.ANTIALIAS)
-    result_image.resize((1200, 1200))
-    print(result_image.size)
-    bgimage = Image.new('L', (1200, 200))
-    print(bgimage.size)
-    result_image = Image.blend(bgimage, result_image, 0.6)
-    result_image.save('result.jpg', file_format="jpg", dpi=(300, 300))
-    return result_image
-   
+    
+    result_image.save('result.png')
 
 
